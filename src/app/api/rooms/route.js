6e41152b14fd76cd1ebcda3c4f5da6a7cf8dd8a5
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-03-05 22:51:30 +0500

    new room handler

--- src/app/api/rooms/route.js
+++ src/app/api/rooms/route.js
@@ -1,9 +1,11 @@
 import nc from 'next-connect';
 import dbConnect from '../../../../config/dbConnect';
-import { allRooms } from '@/app/controllers/roomControllers';
+import { allRooms, newRoom } from '@/app/controllers/roomControllers';
 
 const handler = nc();
 dbConnect();
+
 handler.get(allRooms);
+handler.post(newRoom);
 
 export default handler;
