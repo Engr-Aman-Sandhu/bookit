41ec9e0f4b273184a7892d8cc81295bc5ceb47af
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-03-10 23:41:17 +0500

    update a Room Controller Function

--- src/app/controllers/roomControllers.js
+++ src/app/controllers/roomControllers.js
@@ -63,4 +63,38 @@ const getSingleRoom = async (req, res) => {
     room,
   });
 };
-export { allRooms, newRoom, getSingleRoom };
+
+const updateRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+    if (!room) {
+      res.status(404).json({
+        success: false,
+        error: 'Room not found with this ID',
+      });
+    }
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+  const room = await Room.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    room,
+  });
+};
+
+export { allRooms, newRoom, getSingleRoom, updateRoom };
