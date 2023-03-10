fbfde146a0dbddbea41256bf0cf52894e50f8f2c
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-03-10 23:33:49 +0500

    Get Single Room Controller Function

--- src/app/controllers/roomControllers.js
+++ src/app/controllers/roomControllers.js
@@ -36,4 +36,31 @@ const newRoom = async (req, res) => {
     room,
   });
 };
-export { allRooms, newRoom };
+
+const getSingleRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+    if (!room) {
+      res.status(404).json({
+        success: false,
+        error: 'Room  not found with this ID',
+      });
+    }
+    res.status(200).json({
+      success: true,
+      room,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+  const room = await Room.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    room,
+  });
+};
+export { allRooms, newRoom, getSingleRoom };
