f4521fc95e31040ee420be92d7fa5ce1a5151eae
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-03-14 23:29:26 +0500

    error removal

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,23 +1,23 @@
 import ErrorHandler from '../utils/errorHandler';
 
-export default (err, req, res, next) => {
+const errors = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
 
   let error = { ...err };
 
   error.message = err.message;
 
-  //   // Wrong Mongoose Object ID Error
-  //   if (err.name === 'CastError') {
+  // // Wrong Mongoose Object ID Error
+  // if (err.name === 'CastError') {
   //     const message = `Resource not found. Invalid: ${err.path}`;
   //     error = new ErrorHandler(message, 400);
-  //   }
+  // }
 
-  //   // Handling mongoose Validation error
-  //   if (err.name === 'ValidationError') {
-  //     const message = Object.values(err.errors).map((value) => value.message);
+  // // Handling mongoose Validation error
+  // if (err.name === 'ValidationError') {
+  //     const message = Object.values(err.errors).map(value => value.message);
   //     error = new ErrorHandler(message, 400);
-  //   }
+  // }
 
   res.status(err.statusCode).json({
     success: false,
@@ -26,3 +26,5 @@ export default (err, req, res, next) => {
     stack: error.stack,
   });
 };
+
+export default errors;
